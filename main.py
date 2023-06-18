224e6dbd5b50603c55355236ab3cc876fee462f6
João Vaz <119084806+jg2alv@users.noreply.github.com>
2023-06-18 12:51:02 -0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,50 @@
+import argparse
+from pypdf import PdfReader, PdfWriter
+
+def main() -> int:
+    parser = argparse.ArgumentParser(prog='pythreader')
+    parser.add_argument('files', type=str, nargs='+')
+    args = parser.parse_args()
+    
+    merger = PdfWriter()
+    remover = PdfWriter()
+
+    for file in args.files:
+        merger.append(file)
+
+    merger.write('result.pdf')
+    merger.close()
+
+    reader = PdfReader('result.pdf')
+    numpages = len(reader.pages)
+    pagerange = '1-{}'.format(numpages)
+    prompt = 'Pages to remove (comma-separated numbers from {}): '.format(pagerange)
+    prompt = input(prompt)
+    pagestoremove = []
+
+    for page in prompt.split(','):
+        if not page.isdigit():
+            print("'{}' is not a valid page number".format(page))
+            return 1
+            
+        idx = int(page) - 1
+        if idx >= numpages:
+            print("'{}' is out of range {}".format(page, pagerange))
+            return 1
+
+        pagestoremove.append(idx)
+
+    for i in range(numpages):
+        if i in pagestoremove:
+            continue
+
+        page = reader.pages[i]
+        remover.add_page(page)
+
+    remover.write('result-removed.pdf')
+    remover.close()
+
+    return 0
+
+if __name__ == '__main__':
+    main()
